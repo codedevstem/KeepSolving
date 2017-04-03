ef2f95cae0bf2e6cb7a1396d91f453cc34f9c4f9
Kristian Os <codedevstem@gmail.com>
2017-04-03 22:50:06 +0200

    more progress

--- jsScripts/simpleWires.js
+++ jsScripts/simpleWires.js
@@ -1,7 +1,21 @@
+let wiresSection;
 function fillInSimpleWires(solveSection) {
- let resetButton = document.createElement("input");
+    let resetButton = document.createElement("input");
     resetButton.value = "Reset Module";
-    resetButton.addEventListener("click", function () {resetModule()}, false)
+    resetButton.readOnly = true;
+    resetButton.addEventListener("click", function() {resetSimpleModule()}, false)
     solveSection.appendChild(resetButton);
-
+    let wiresSection = document.createElement("section");
+    wiresSection.id = "wireSection";
+    let wireHeader = document.createElement("p");
+    wireHeader.innerHTML = "How many wires are there?";
+    wiresSection.appendChild(wireHeader);
+    let wireChoiceForm = document.createElement("form");
+    wireChoiceForm.id = "wireForm";
+    for(let i = 3; i >= 6; i++){
+        let numWiresChoice = document.createElement("input");
+        numWiresChoice.type = "button";
+        numWiresChoice.value = "i";
+        
+    }
 }
